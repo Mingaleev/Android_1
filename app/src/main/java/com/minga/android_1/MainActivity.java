bd131b2524b96cfc03463f626ea6d76e05ad8c72
Mingaleev <art.mingaleev@yandex.ru>
2021-01-28 18:21:51 +0300

    добавил TextView

--- app/src/main/java/com/minga/android_1/MainActivity.java
+++ app/src/main/java/com/minga/android_1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.minga.android_1;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = findViewById(R.id.tv_top);
     }
 }
